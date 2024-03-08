8878ed9524e7f3ec62a0ea2eb87ad10c82060b1b
yongho9064 <dydgmldydgh@naver.com>
2024-03-08 18:55:10 +0900

    :recycle: 패키지 구조화 및 @Mapper 적용

--- src/main/java/com/everypet/member/service/impl/MemberServiceImpl.java
+++ src/main/java/com/everypet/member/service/impl/MemberServiceImpl.java
@@ -1,16 +1,16 @@
 package com.everypet.member.service.impl;
 
-import com.everypet.member.domain.Member;
+import com.everypet.member.data.domain.Member;
 import com.everypet.member.service.MemberService;
 
 import java.util.List;
 
 public class MemberServiceImpl implements MemberService {
-    public List<Member> getMemberAll() {
+    public List<Member> selectMemberAll() {
         return null;
     }
 
-    public Member getMember(String member_id) {
+    public Member selectMemberById(String member_id) {
         return null;
     }
 }
