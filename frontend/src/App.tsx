5ae657a71d716fb5d06b43315f71a6c8bf5516a8
Leekwanyong <dlrkdid12@gmail.com>
2024-03-09 18:20:23 +0900

    :lipstick: 로그인 토큰 코드

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,12 +3,49 @@ import './App.css';
 import axios from 'axios';
 
 function App() {
-    const [hi, setHi] = useState('');
+    const [id, setId] = useState('');
+    const [pw, setPw] = useState('');
+    const [message, setMessage] = useState('');
+    const [csrfToken, setCsrfToken] = useState('');
 
     useEffect(() => {
-        axios.get('/api/test').then((res) => setHi(res.data));
+        const fetchCsrfToken = async () => {
+            try {
+                const csrf = await axios.get('토큰');
+                setCsrfToken(csrf.data.csrfToken)
+            } catch (e: any) {
+                console.log('csrfToken No', e)
+            }
+        }
+        fetchCsrfToken();
     }, []);
-    return <div className="App">BackEnd {hi}</div>;
+
+    const handleLogin = async (e: any) => {
+        e.preventDefault();
+        try {
+            const response = await axios.post('/signin', {id, pw},
+                {
+                headers: {
+                    'X-CSRF-Token': csrfToken,
+                }});
+            setMessage(response.data.message)
+        }catch (e: any) {
+            setMessage(e.response.data.message)
+        }
+    }
+
+    return <div>
+        <form action='/signin' onSubmit={handleLogin}>
+            <label htmlFor='meber_id'>
+                <input type='text' value={id} name='meber_id' onChange={(e) => setId(e.target.value)}/>
+            </label>
+            <label htmlFor='meber_pwd'>
+                <input type='password' value={pw} name='meber_pwd' onChange={(e) => setPw(e.target.value)}/>
+            </label>
+            <button type='submit'>Login</button>
+        </form>
+        <p>{message}</p>
+    </div>;
 }
 
 export default App;
