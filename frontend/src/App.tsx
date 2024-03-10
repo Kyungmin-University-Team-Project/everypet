3146f92ab8dd45bcf1072b0b8610ca445d9c316d
gksktl111 <gksktl111@naver.com>
2024-03-10 17:26:18 +0900

    :sparkles: 스프링 시큐리티 연결후 테스트 확인완료

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,37 +1,20 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import React from 'react';
 
 function App() {
-  const [hello, setHello] = useState('');
-
-  useEffect(() => {
-    axios.get('/test').then((res) => {
-      setHello(res.data);
-    });
-  }, []);
-
-  const handleLogin = (e: any) => {
-    e.preventDefault();
-
-    console.log('asd');
-  };
-
   return (
-    <div>
-      <form onSubmit={handleLogin}>
-        <label>
-          아이디
-          <input type='text' id='member_id' name='member_id' />
-        </label>
-        <label htmlFor='member_pwd'>
-          비밀번호
-          <input type='password' id='member_pwd' name='member_pwd' />
-        </label>
-        <button type='submit' className='submit-btn'>
-          제출
-        </button>
-      </form>
-    </div>
+      <div>
+        <form action="/signin" method="post">
+          <label>
+            아이디
+            <input type='text' id='member_id' name='member_id'/>
+          </label>
+          <label htmlFor='member_pwd'>
+            비밀번호
+            <input type='password' id='member_pwd' name='member_pwd'/>
+          </label>
+          <button type='submit' className='submit-btn'>제출</button>
+        </form>
+      </div>
   );
 }
 
