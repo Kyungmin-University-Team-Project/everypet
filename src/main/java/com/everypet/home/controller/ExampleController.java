b8c72d8090322af1c0af60a40964f56e40c98b23
gksktl111 <gksktl111@naver.com>
2024-03-10 17:27:46 +0900

    :recycle: 쓸모없는 코드 제거

--- src/main/java/com/everypet/home/controller/ExampleController.java
+++ src/main/java/com/everypet/home/controller/ExampleController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ExampleController {
-    @GetMapping("/api/test")
+    @GetMapping("/test")
     public String hello() {
         return "zzzzzzzz";
     }
