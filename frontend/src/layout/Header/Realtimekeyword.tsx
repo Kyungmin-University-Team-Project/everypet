cf4a54e5bc6ead59e350ef632b567d6ebbcd119c
pointtt <50075024+gksktl111@users.noreply.github.com>
2024-03-22 16:31:53 +0900

    ✨  실시간 검색어 기능 추가 및 랭킹 mock 데이터 추가

--- frontend/src/layout/Header/Realtimekeyword.tsx
+++ frontend/src/layout/Header/Realtimekeyword.tsx
@@ -2,30 +2,50 @@ import React, { useEffect, useState } from 'react';
 import styles from './Realtimekeyword.module.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
+import { Ranking } from '../../typings/header';
 
 const Realtimekeyword = () => {
-  const [number, setNumber] = useState(1);
+  const [rankings, setRankings] = useState<Ranking[]>([]);
+
+  const [currentRank, setCurrentRank] = useState(1);
 
   useEffect(() => {
-    const intervalId = setInterval(() => {
-      setNumber((prevNumber) => prevNumber + 1);
+    // mock 데이터 로드
+    fetch('/mock/real_rank.json')
+      .then((response) => response.json())
+      .then((data) => setRankings(data))
+      .catch((error) => console.error('Error fetching real_rank.json:', error));
 
-      if (number >= 10) {
-        setNumber(1);
-      }
+    // 실시간 순위 업데이트
+    const intervalId = setInterval(() => {
+      // 1~10 까지
+      setCurrentRank((prevRank) => (prevRank % 10) + 1);
     }, 2000);
 
     return () => clearInterval(intervalId);
-  }, [number]);
+  }, []);
 
+  // 실시간 검색어 모달창 열기
   const openRankModal = () => {
     console.log('모달창 열기');
   };
 
+  const currentRanking = rankings.find((item) => item.rank === currentRank);
+
+  // 실시간 검색어 누르면 해당 키워드로 검색
+  const searchKeyword = () => {
+    console.log(currentRanking?.rank, currentRanking?.keyword);
+  };
+
   return (
     <div className={styles.container}>
-      <span className={styles.keyword}>
-        <a>{number}</a> 실시간 검색어
+      <span className={styles.rank__container} onClick={searchKeyword}>
+        {currentRanking && (
+          <>
+            <span className={styles.rank}>{currentRanking.rank}</span>
+            <a className={styles.keyword}>{currentRanking.keyword}</a>
+          </>
+        )}
       </span>
       <FontAwesomeIcon
         icon={faChevronDown}
