bbb6ffff922c7d3a0ab4a34035dd7aa98279bd79
pointtt <50075024+gksktl111@users.noreply.github.com>
2024-03-22 15:52:57 +0900

    ✨ setInterval로 순위 기능 추가

--- frontend/src/layout/Header/Realtimekeyword.tsx
+++ frontend/src/layout/Header/Realtimekeyword.tsx
@@ -1,10 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './Realtimekeyword.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
 
 const Realtimekeyword = () => {
+  const [number, setNumber] = useState(1);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setNumber((prevNumber) => prevNumber + 1);
+
+      if (number >= 10) {
+        setNumber(1);
+      }
+    }, 2000);
+
+    return () => clearInterval(intervalId);
+  }, [number]);
+
+  const openRankModal = () => {
+    console.log('모달창 열기');
+  };
+
   return (
     <div className={styles.container}>
-      <span className={styles.keyword}>실시간 검색어</span>
+      <span className={styles.keyword}>
+        <a>{number}</a> 실시간 검색어
+      </span>
+      <FontAwesomeIcon
+        icon={faChevronDown}
+        onClick={openRankModal}
+        className={styles.open}
+      />
     </div>
   );
 };
