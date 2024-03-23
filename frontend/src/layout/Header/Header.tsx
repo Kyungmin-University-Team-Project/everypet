33a0bb271164a524f7a8891a5b1b8e77f2957121
gksktl111 <gksktl111@naver.com>
2024-03-23 17:33:38 +0900

    🩹 사소한 css 수정

--- frontend/src/layout/Header/Header.tsx
+++ frontend/src/layout/Header/Header.tsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import styles from './Header.module.css';
 import { Link } from 'react-router-dom';
-import Searchinput from './Searchinput';
 import Realtimekeyword from './Realtimekeyword';
 import Cart from './Cart';
+import Searchinput from './Searchinput';
+import Orderview from './Orderview';
 
 const Header = () => {
   return (
     <header className={styles.container}>
       <div className={styles.inner}>
         <Link to='/' className={styles.title}>
-          <a>에브리펫</a>
+          에브리펫
         </Link>
 
         <Searchinput />
@@ -18,10 +19,12 @@ const Header = () => {
         <Realtimekeyword />
 
         <Link to='/login' className={styles.login__btn}>
-          <a>로그인</a>
+          로그인
         </Link>
 
         <Cart />
+
+        <Orderview />
       </div>
     </header>
   );
