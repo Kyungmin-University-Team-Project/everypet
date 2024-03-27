63ecdc1e8d6ed1039971bff7911262adf450405a
pointtt <50075024+gksktl111@users.noreply.github.com>
2024-03-27 15:17:42 +0900

    💄 카테고리 UI 수정, 토글 문제 발생 해결해야함

--- frontend/src/layout/category/Categorysidebar.tsx
+++ frontend/src/layout/category/Categorysidebar.tsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import styles from './Categorysidebar.module.css';
 import { RootState } from '../../redux/store/rootReducer';
 import { setClickedCategory } from '../../redux/features/categorySlice';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faX } from '@fortawesome/free-solid-svg-icons';
+import { closeSidebar, toggleSidebar } from '../../redux/features/sidebarSlice';
 
 const categories = [
   { name: '쿠폰/기획전', link: '/page1' },
@@ -15,32 +18,60 @@ const categories = [
   { name: '파충류', link: '/page7' },
 ];
 
-const CategorySidebar = () => {
+const CategorySidebar = ({ isOpen }: { isOpen: boolean }) => {
   const clickedCategory = useSelector(
     (state: RootState) => state.category.clickedCategory
   );
   const dispatch = useDispatch();
+
+  useEffect(() => {
+    console.log(isOpen);
+
+    const handleClickOutside = (event: any) => {
+      const sidebar = document.querySelector(`.${styles.container}`);
+      if (sidebar && !sidebar.contains(event.target)) {
+        dispatch(toggleSidebar()); // 토글 액션 디스패치
+      }
+    };
+
+    document.addEventListener('click', handleClickOutside);
+
+    return () => {
+      document.removeEventListener('click', handleClickOutside);
+    };
+  }, [dispatch]);
 
   const handleClick = (category: string) => {
     dispatch(setClickedCategory(category));
   };
 
   return (
-    <div className={styles.sidebar}>
-      <h2>Categories</h2>
-      <ul>
-        {categories.map((category, index) => (
-          <li key={index}>
-            <Link
-              to={category.link}
-              className={clickedCategory === category.name ? styles.active : ''}
-              onClick={() => handleClick(category.name)}
-            >
-              {category.name}
-            </Link>
-          </li>
-        ))}
-      </ul>
+    <div className={isOpen ? styles.containerOpen : styles.containerClosed}>
+      <div className={styles.container}>
+        <header className={styles.header}>
+          <span className={styles.logo}>카테고리</span>
+          <FontAwesomeIcon
+            icon={faX}
+            className={styles.close__btn}
+            onClick={() => dispatch(closeSidebar())}
+          />
+        </header>
+        <ul className={styles.ul}>
+          {categories.map((category, index) => (
+            <li className={styles.li} key={index}>
+              <Link
+                to={category.link}
+                className={
+                  clickedCategory === category.name ? styles.active : ''
+                }
+                onClick={() => handleClick(category.name)}
+              >
+                {category.name}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
