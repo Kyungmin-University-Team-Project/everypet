125f5dc6d66a474af75a7e25d3d8f23dbb155706
pointtt <50075024+gksktl111@users.noreply.github.com>
2024-03-27 12:00:58 +0900

    💄 active 상태 관리를 리덕스로 변경 및 카테고리 사이드바 UI 기초 작업

--- frontend/src/layout/category/Categorysidebar.tsx
+++ frontend/src/layout/category/Categorysidebar.tsx
@@ -1,28 +1,45 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
 import styles from './Categorysidebar.module.css';
+import { RootState } from '../../redux/store/rootReducer';
+import { setClickedCategory } from '../../redux/features/categorySlice';
+
+const categories = [
+  { name: '쿠폰/기획전', link: '/page1' },
+  { name: '타임딜', link: '/page2' },
+  { name: '강아지', link: '/page3' },
+  { name: '고양이', link: '/page4' },
+  { name: '설치류', link: '/page5' },
+  { name: '조류', link: '/page6' },
+  { name: '파충류', link: '/page7' },
+];
 
 const CategorySidebar = () => {
+  const clickedCategory = useSelector(
+    (state: RootState) => state.category.clickedCategory
+  );
+  const dispatch = useDispatch();
+
+  const handleClick = (category: string) => {
+    dispatch(setClickedCategory(category));
+  };
+
   return (
     <div className={styles.sidebar}>
       <h2>Categories</h2>
       <ul>
-        <li>
-          <a href='#' className={styles.active}>
-            Category 1
-          </a>
-        </li>
-        <li>
-          <a href='#'>Category 2</a>
-        </li>
-        <li>
-          <a href='#'>Category 3</a>
-        </li>
-        <li>
-          <a href='#'>Category 4</a>
-        </li>
-        <li>
-          <a href='#'>Category 5</a>
-        </li>
+        {categories.map((category, index) => (
+          <li key={index}>
+            <Link
+              to={category.link}
+              className={clickedCategory === category.name ? styles.active : ''}
+              onClick={() => handleClick(category.name)}
+            >
+              {category.name}
+            </Link>
+          </li>
+        ))}
       </ul>
     </div>
   );
--- frontend/src/layout/category/Productcategory.tsx
+++ frontend/src/layout/category/Productcategory.tsx
@@ -1,9 +1,12 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { useDispatch, useSelector } from 'react-redux';
 import ProductcategoryItem from './ProductcategoryItem';
 import styles from './Productcategory.module.css';
+import { setClickedCategory } from '../../redux/features/categorySlice';
+import { RootState } from '../../redux/store/rootReducer';
 import CategorySidebar from './Categorysidebar';
 
 const categories = [
@@ -17,20 +20,23 @@ const categories = [
 ];
 
 const Productcategory = () => {
-  const [clickedCategory, setClickedCategory] = useState('');
+  const dispatch = useDispatch();
+  const clickedCategory = useSelector(
+    (state: RootState) => state.category.clickedCategory
+  );
   const location = useLocation();
 
   useEffect(() => {
     if (location.pathname === '/') {
-      setClickedCategory('');
+      dispatch(setClickedCategory(''));
     }
-  }, [location.pathname]);
+  }, [location.pathname, dispatch]);
 
   const handleClick = (category: string) => {
     if (clickedCategory === category) {
       return;
     } else {
-      setClickedCategory(category);
+      dispatch(setClickedCategory(category));
     }
   };
 
