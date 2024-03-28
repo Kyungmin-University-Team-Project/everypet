09fe689ceba619a6ddd2ca3ef331dadda39f1e53
yongho9064 <dydgmldydgh@naver.com>
2024-03-29 04:34:53 +0900

    :truck: JWTFilter.java 이동

--- src/main/java/com/everypet/auth/jwt/filter/JWTFilter.java
+++ src/main/java/com/everypet/auth/jwt/filter/JWTFilter.java
@@ -1,7 +1,7 @@
-package com.everypet.util.jwt.filter;
+package com.everypet.auth.jwt.filter;
 
+import com.everypet.auth.jwt.util.JWTManager;
 import com.everypet.member.data.domain.Member;
-import com.everypet.util.jwt.factory.JWTFactory;
 import io.jsonwebtoken.ExpiredJwtException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -24,7 +24,7 @@ import java.util.List;
 @RequiredArgsConstructor
 public class JWTFilter extends OncePerRequestFilter {
 
-    private final JWTFactory jwtFactory;
+    private final JWTManager jwtManager;
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         // 헤더에서 access키에 담긴 토큰을 꺼냄
@@ -38,7 +38,7 @@ public class JWTFilter extends OncePerRequestFilter {
 
         // 토큰 만료 여부 확인, 만료시 다음 필터로 넘기지 않음
         try {
-            jwtFactory.isExpired(accessToken);
+            jwtManager.isExpired(accessToken);
         } catch (ExpiredJwtException e) {
 
             //response body
@@ -50,7 +50,7 @@ public class JWTFilter extends OncePerRequestFilter {
         }
 
         // 토큰이 access인지 확인 (발급시 페이로드에 명시)
-        String category = jwtFactory.getCategory(accessToken);
+        String category = jwtManager.getCategory(accessToken);
 
         if (!category.equals("access")) {
 
@@ -64,9 +64,9 @@ public class JWTFilter extends OncePerRequestFilter {
         }
 
         // username, role 값을 획득
-        String username = jwtFactory.getUsername(accessToken);
+        String username = jwtManager.getUsername(accessToken);
         List<String> role = new ArrayList<>(
-                Arrays.asList(jwtFactory.getRole(accessToken))
+                Arrays.asList(jwtManager.getRole(accessToken))
         );
 
 
