b8a6fce2f63c1a9f9c2cde3bceed3d9fee727a98
yongho9064 <dydgmldydgh@naver.com>
2024-04-05 08:30:08 +0900

    :recycle: 필요없는 import 제거 및 SecurityConfig 수정

--- src/main/java/com/everypet/config/AppConfig.java
+++ src/main/java/com/everypet/config/AppConfig.java
@@ -1,9 +1,7 @@
 package com.everypet.config;
 
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.stereotype.Component;
 
 @Configuration
 @Import({DatabaseConfig.class, DatabaseConfig.class, MvcConfig.class, SecurityConfig.class, SwaggerConfig.class})
