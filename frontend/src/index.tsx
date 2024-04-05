837b181cc1536c9e81ccdf738c854ccabbf069bb
pointtt <50075024+gksktl111@users.noreply.github.com>
2024-04-05 16:11:46 +0900

    🎨 라우트 구조 변경

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,44 +1,44 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import reportWebVitals from "./reportWebVitals";
-import { Provider } from "react-redux"; // Redux의 Provider import
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Pages1 from "./pages/category/Pages1";
-import Home from "./pages/home/Home";
-import Pages3 from "./pages/category/Pages3";
-import Pages2 from "./pages/category/Pages2";
-import Login from "./pages/auth/Login";
-import Signup from "./pages/auth/Signup";
-import Findauth from "./pages/auth/Findauth";
-import Pages7 from "./pages/category/Pages7";
-import Pages6 from "./pages/category/Pages6";
-import Pages5 from "./pages/category/Pages5";
-import Pages4 from "./pages/category/Pages4";
-import store from "./redux/store/store";
-import Agreement from "./pages/auth/Agreement";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux'; // Redux의 Provider import
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Pages1 from './pages/category/Pages1';
+import Root from './pages/home/Root';
+import Pages3 from './pages/category/Pages3';
+import Pages2 from './pages/category/Pages2';
+import Login from './pages/auth/Login';
+import Signup from './pages/auth/Signup';
+import Findauth from './pages/auth/Findauth';
+import Pages7 from './pages/category/Pages7';
+import Pages6 from './pages/category/Pages6';
+import Pages5 from './pages/category/Pages5';
+import Pages4 from './pages/category/Pages4';
+import store from './redux/store/store';
+import Agreement from './pages/auth/Agreement';
 
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <Home />,
+    path: '/',
+    element: <Root />,
     children: [
-      { path: "page1", element: <Pages1 /> },
-      { path: "page2", element: <Pages2 /> },
-      { path: "page3", element: <Pages3 /> },
-      { path: "page4", element: <Pages4 /> },
-      { path: "page5", element: <Pages5 /> },
-      { path: "page6", element: <Pages6 /> },
-      { path: "page7", element: <Pages7 /> },
+      { path: 'page1', element: <Pages1 /> },
+      { path: 'page2', element: <Pages2 /> },
+      { path: 'page3', element: <Pages3 /> },
+      { path: 'page4', element: <Pages4 /> },
+      { path: 'page5', element: <Pages5 /> },
+      { path: 'page6', element: <Pages6 /> },
+      { path: 'page7', element: <Pages7 /> },
     ],
   },
   {
-    path: "/login",
+    path: '/login',
     element: <Login />,
     children: [
-      { path: "agreement", element: <Agreement /> },
-      { path: "signup", element: <Signup /> },
-      { path: "forgot-password", element: <Findauth /> },
+      { path: 'agreement', element: <Agreement /> },
+      { path: 'signup', element: <Signup /> },
+      { path: 'forgot-password', element: <Findauth /> },
     ],
   },
 ]);
@@ -49,7 +49,7 @@ ReactDOM.render(
       <RouterProvider router={router} />
     </Provider>
   </React.StrictMode>,
-  document.getElementById("root"),
+  document.getElementById('root')
 );
 
 reportWebVitals();
---
+++ frontend/src/pages/home/Root.tsx
@@ -0,0 +1,39 @@
+import React, { useEffect } from 'react';
+import { Outlet, useLocation } from 'react-router-dom';
+import Productcategory from '../../layout/category/Productcategory';
+import Header from '../../layout/Header/Header';
+import Home from './Home';
+
+const Root = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    const beforeUnloadHandler = (event: any) => {
+      event.preventDefault();
+      event.returnValue = ''; // 브라우저에 표시되는 메시지입니다.
+      // 여기에 실행시키고자 하는 함수를 추가합니다.
+      // 예: console.log("브라우저를 닫는다!");
+
+      // 서버로 디비 토큰 삭제 요청
+      // 로컬의 쿠키 삭제
+    };
+
+    // beforeunload 이벤트 리스너 추가
+    window.addEventListener('beforeunload', beforeUnloadHandler);
+
+    // 컴포넌트가 언마운트될 때 이벤트 리스너 제거
+    return () => {
+      window.removeEventListener('beforeunload', beforeUnloadHandler);
+    };
+  }, []);
+
+  return (
+    <>
+      <Header />
+      <Productcategory />
+      {location.pathname === '/' ? <Home /> : <Outlet />}
+    </>
+  );
+};
+
+export default Root;
