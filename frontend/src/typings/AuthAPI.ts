88576e0ba244104e2737422361aa53126bd954a7
gksktl111 <gksktl111@naver.com>
2024-04-28 04:15:32 +0900

    🐛 쿠키 저장 문제해결

--- frontend/src/typings/AuthAPI.ts
+++ frontend/src/typings/AuthAPI.ts
@@ -1,4 +1,4 @@
-import axios from "axios";
+import axios from 'axios';
 
 interface ResponseData {
   access: string;
@@ -13,22 +13,22 @@ export const login = async ({
 }): Promise<ResponseData> => {
   const data = { memberId, memberPwd };
   try {
-    const response = await axios.post("http://localhost:8080/signin", data);
+    const response = await axios.post('/signin', data);
     const responseData: ResponseData = {
       access: response.headers.access,
     };
 
     // 서버 응답 데이터 확인
-    console.log("Server Response Data:", responseData);
+    console.log('Server Response Data:', responseData);
 
     // 토큰이 올바르게 추출되었는지 확인
     if (!responseData.access) {
-      console.log("Invalid response data received from server");
+      console.log('Invalid response data received from server');
     }
 
     return responseData; // 정상적인 경우 반환
   } catch (error) {
-    console.error("Error in login:", error);
+    console.error('Error in login:', error);
     throw error; // 오류가 발생한 경우 오류를 throw
   }
 };
@@ -42,6 +42,6 @@ export const signUpLogin = async ({
   memberPwd: string;
 }): Promise<ResponseData> => {
   const data = { memberId, memberPwd };
-  const response = await axios.post("http://localhost:8080/signup", data);
+  const response = await axios.post('http://localhost:8080/signup', data);
   return response.data;
 };
