955a9bd29addd3dc899b697650c30064d7a291be
yongho9064 <dydgmldydgh@naver.com>
2024-05-24 06:17:46 +0900

    :wrench: gooleCloud 설정 파일 추가

---
+++ src/main/java/com/everypet/global/config/GoogleCloudStorageConfig.java
@@ -0,0 +1,27 @@
+package com.everypet.global.config;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.storage.Storage;
+import com.google.cloud.storage.StorageOptions;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.IOException;
+
+@Configuration
+public class GoogleCloudStorageConfig {
+
+    @Bean
+    public Storage storage() throws IOException {
+
+        ClassPathResource resource = new ClassPathResource("every-pet-418208-be0b25df67a9.json");
+        GoogleCredentials credentials = GoogleCredentials.fromStream(resource.getInputStream());
+        String projectId = "every-pet-418208";
+        return StorageOptions.newBuilder()
+                .setProjectId(projectId)
+                .setCredentials(credentials)
+                .build()
+                .getService();
+    }
+}
