8466ae4c702a9e3a285d8bcbe8f1c79cc2940389
gksktl111 <gksktl111@naver.com>
2024-05-27 00:19:59 +0900

    💄 로그인, 회원가입, 고객센터 버튼 컴포넌트로 분리

--- frontend/src/layout/Header/Header.tsx
+++ frontend/src/layout/Header/Header.tsx
@@ -1,16 +1,18 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React from 'react';
 import styles from './Header.module.css';
 import { Link } from 'react-router-dom';
 import useToggle from '../../utils/category/ToggleUtil';
 import Categorymodal from '../category/Categorymodal';
 import Searchinput from './Searchinput';
 import Usermenu from './Usermenu';
+import TopMenu from './TopMenu';
 
 const Header = () => {
   const [isOpen, toggleOn, toggleOff] = useToggle(false);
 
   return (
     <>
+      <TopMenu />
       <header className={styles.container}>
         <div className={styles.inner}>
           <Link to='/' className={styles.title}>
