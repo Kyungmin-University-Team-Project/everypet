3cec661abe34f5aa54c33255c75a585449956af9
gksktl111 <gksktl111@naver.com>
2024-05-27 00:21:18 +0900

    🩹 코드정리

---
+++ frontend/src/pages/usermenu/CustomerService.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Fixedheader from '../../layout/Header/Fixedheader';
+import Header from '../../layout/Header/Header';
+import Productcategory from '../../layout/category/Productcategory';
+
+const CustomerService = () => {
+  return (
+    <div>
+      <Fixedheader />
+      <Header />
+      <Productcategory />
+    </div>
+  );
+};
+
+export default CustomerService;
