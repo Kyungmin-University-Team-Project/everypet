7e5331b24d94e3a74691182fa74e88b8aa9fd682
pointtt <50075024+gksktl111@users.noreply.github.com>
2024-05-27 16:48:04 +0900

    feat: Update 타임딜 컴포넌트, 페이지 UI 수정정

--- frontend/src/pages/category/TimeDeal.tsx
+++ frontend/src/pages/category/TimeDeal.tsx
@@ -3,8 +3,9 @@ import styles from './TimeDeal.module.css';
 import Header from '../../layout/Header/Header';
 import Fixedheader from '../../layout/Header/Fixedheader';
 import Productcategory from '../../layout/category/Productcategory';
-import ItemList from '../../components/common/ItemList';
-import DetailedCategory from '../../components/common/DetailedCategory';
+import TimeItem from '../../components/timedeal/TimeItem';
+import TimeItemList from '../../components/timedeal/TimeItemList';
+import Footer from '../../components/home/Footer';
 
 const TimeDeal = ({ category }: any) => {
   useEffect(() => {
@@ -22,13 +23,12 @@ const TimeDeal = ({ category }: any) => {
       <div className={styles.container}>
         <div className={styles.inner}>
           {/* 아이템 리스트에 아이템 정보 넣기 */}
-
-          <div>asdad</div>
-          <div>asdad</div>
-          <div>asdad</div>
-          <ItemList />
+          <span className={styles.title}>타임딜</span>
+          <TimeItemList />
         </div>
       </div>
+
+      <Footer />
     </div>
   );
 };
