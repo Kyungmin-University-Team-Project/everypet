889d046a0162e53d01e44306bdb7c7784c5177db
Leekwanyong <dlrkdid12@kyungmin.ac.kr>
2024-05-28 12:12:28 +0900

    feat: Add item navigate로 받은 데이터 UI표시

--- frontend/src/pages/moreInformation/MoreInformation.tsx
+++ frontend/src/pages/moreInformation/MoreInformation.tsx
@@ -2,13 +2,34 @@ import React from 'react';
 import Header from "../../layout/Header/Header";
 import Fixedheader from "../../layout/Header/Fixedheader";
 import Productcategory from "../../layout/category/Productcategory";
+import {useLocation} from "react-router-dom";
+import {FaStar, FaRegStar} from "react-icons/fa";
 
 const MoreInformation = () => {
+    const location = useLocation();
+    const {item} = location.state;
+
+    const renderStars = (rating: number) => {
+        const stars = [];
+        for (let i = 1; i <= 5; i++) {
+            stars.push(i <= rating ? <FaStar key={i} /> : <FaRegStar key={i} />);
+        }
+        return stars;
+    };
+
     return (
         <div>
             <Fixedheader/>
             <Header/>
             <Productcategory/>
+            <div >
+                <img src={item.imageUrl} alt={item.name}  />
+                <h2>{item.name}</h2>
+                <p>Price: {item.price}원</p>
+                <p>Discount: {item.discount}%</p>
+                <p>Recommended: <div >{renderStars(item.recommended)}</div></p>
+                <p>Review Count: {item.reviewCount}</p>
+            </div>
         </div>
     );
 };
