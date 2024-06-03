a000d2885987a5538f5b50a6fff453e528809521
pointtt <50075024+gksktl111@users.noreply.github.com>
2024-06-03 13:27:15 +0900

    feat: Fixedheader 업데이트

--- frontend/src/layout/Header/Fixedheader.tsx
+++ frontend/src/layout/Header/Fixedheader.tsx
@@ -1,59 +1,60 @@
-import React, { useEffect, useState } from 'react';
+import React, {useEffect, useState} from 'react';
 import styles from './Fixedheader.module.css';
 import Categorymodal from '../category/Categorymodal';
 import Usermenu from './Usermenu';
 import Searchinput from './Searchinput';
-import { Link } from 'react-router-dom';
+import {Link} from 'react-router-dom';
 import useToggle from '../../utils/category/ToggleUtil';
 import Categorybarbtn from '../category/Categorybarbtn';
 import TopMenu from './TopMenu';
 
 const Fixedheader = () => {
-  const [isOpen, toggleOn, toggleOff] = useToggle(false);
+    const [isOpen, toggleOn, toggleOff] = useToggle(false);
 
-  const [scrollY, setScrollY] = useState(0);
+    const [scrollY, setScrollY] = useState(0);
 
-  useEffect(() => {
-    const handleScroll = () => {
-      setScrollY(window.scrollY); // 스크롤 Y 값을 업데이트
-      if (isOpen) {
-        toggleOff(); // 스크롤 시 모달이 열려있으면 닫기
-        console.log('성공');
-      }
-    };
+    useEffect(() => {
+        const handleScroll = () => {
+            setScrollY(window.scrollY); // 스크롤 Y 값을 업데이트
+            if (isOpen) {
+                toggleOff(); // 스크롤 시 모달이 열려있으면 닫기
+                console.log('성공');
+            }
+        };
 
-    window.addEventListener('scroll', handleScroll);
+        window.addEventListener('scroll', handleScroll);
 
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, [isOpen, toggleOff]);
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, [isOpen, toggleOff]);
 
-  return (
-    <div className={scrollY >= 200 ? styles.open : styles.close}>
-      <header className={styles.container__fixed}>
-        <div className={styles.inner}>
-          <div className={styles.logo__wrap}>
-            <Categorybarbtn
-              active={false}
-              isOpen={isOpen}
-              setOpen={toggleOn}
-              setClose={toggleOff}
-            />
-            <Link to='/' className={styles.title}>
-              에브리펫
-            </Link>
-          </div>
+    return (
+        <div className={scrollY >= 200 ? styles.open : styles.close}>
+            <header className={styles.container__fixed}>
+                <TopMenu/>
+                <div className={styles.inner}>
+                    <div className={styles.logo__wrap}>
+                        <Categorybarbtn
+                            active={false}
+                            isOpen={isOpen}
+                            setOpen={toggleOn}
+                            setClose={toggleOff}
+                        />
+                        <Link to='/' className={styles.title}>
+                            에브리펫
+                        </Link>
+                    </div>
 
-          <Searchinput />
+                    <Searchinput/>
 
-          <Usermenu />
+                    <Usermenu/>
+                </div>
+            </header>
+
+            <Categorymodal isOpen={isOpen} setOpen={toggleOn} setClose={toggleOff}/>
         </div>
-      </header>
-
-      <Categorymodal isOpen={isOpen} setOpen={toggleOn} setClose={toggleOff} />
-    </div>
-  );
+    );
 };
 
 export default Fixedheader;
