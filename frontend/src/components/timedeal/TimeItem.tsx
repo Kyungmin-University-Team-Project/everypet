5a9f9f72e3c8f5ad8a2791b9059a4f19da298061
pointtt <50075024+gksktl111@users.noreply.github.com>
2024-06-03 14:54:03 +0900

    feat: 타임딜 UI 수정

--- frontend/src/components/timedeal/TimeItem.tsx
+++ frontend/src/components/timedeal/TimeItem.tsx
@@ -1,92 +1,96 @@
-import React, { useState, useEffect } from 'react';
+import React, {useState, useEffect} from 'react';
 import styles from './TimeItem.module.css';
-import { FaStar } from 'react-icons/fa';
+import {FaStar} from 'react-icons/fa';
 
 interface Item {
-  timeLeft: number;
-  image: string;
-  discountedPrice: string;
-  originalPrice: string;
-  description: string;
-  rating: number;
-  reviews: string;
-  details: string;
-  coupon: string;
-  couponDescription: string;
+    timeLeft: number;
+    image: string;
+    discountedPrice: string;
+    originalPrice: string;
+    description: string;
+    rating: number;
+    reviews: string;
+    details: string;
+    coupon: string;
+    couponDescription: string;
 }
 
 interface TimeItemProps {
-  item: Item;
+    item: Item;
 }
 
-const TimeItem: React.FC<TimeItemProps> = ({ item }) => {
-  const [timeLeft, setTimeLeft] = useState<number>(item.timeLeft);
-  const [seconds, setSeconds] = useState<string>('');
+const TimeItem: React.FC<TimeItemProps> = ({item}) => {
+    const [timeLeft, setTimeLeft] = useState<number>(item.timeLeft);
+    const [seconds, setSeconds] = useState<string>('');
 
-  useEffect(() => {
-    const formatTime = (seconds: number): string => {
-      const hours = String(Math.floor(seconds / 3600)).padStart(2, '0');
-      const minutes = String(Math.floor((seconds % 3600) / 60)).padStart(
-        2,
-        '0'
-      );
-      const secs = String(seconds % 60).padStart(2, '0');
-      return `${hours}:${minutes}:${secs}`;
+    useEffect(() => {
+        const formatTime = (seconds: number): string => {
+            const hours = String(Math.floor(seconds / 3600)).padStart(2, '0');
+            const minutes = String(Math.floor((seconds % 3600) / 60)).padStart(
+                2,
+                '0'
+            );
+            const secs = String(seconds % 60).padStart(2, '0');
+            return `${hours}:${minutes}:${secs}`;
+        };
+
+        const timer = setInterval(() => {
+            setTimeLeft((prevTime) => {
+                const newTime = prevTime > 0 ? prevTime - 1 : 0;
+                setSeconds(String(newTime % 60).padStart(2, '0'));
+                return newTime;
+            });
+        }, 1000);
+
+        return () => clearInterval(timer);
+    }, []);
+
+    const formatHoursMinutes = (seconds: number): string => {
+        const hours = String(Math.floor(seconds / 3600)).padStart(2, '0');
+        const minutes = String(Math.floor((seconds % 3600) / 60)).padStart(2, '0');
+        return `${hours}:${minutes}`;
     };
 
-    const timer = setInterval(() => {
-      setTimeLeft((prevTime) => {
-        const newTime = prevTime > 0 ? prevTime - 1 : 0;
-        setSeconds(String(newTime % 60).padStart(2, '0'));
-        return newTime;
-      });
-    }, 1000);
-
-    return () => clearInterval(timer);
-  }, []);
-
-  const formatHoursMinutes = (seconds: number): string => {
-    const hours = String(Math.floor(seconds / 3600)).padStart(2, '0');
-    const minutes = String(Math.floor((seconds % 3600) / 60)).padStart(2, '0');
-    return `${hours}:${minutes}`;
-  };
-
-  return (
-    <div className={styles.timeItem}>
-      <div className={styles.body}>
-        <div className={styles.image}>
-          <img src={item.image} alt='Product' />
-        </div>
-        <div className={styles.info}>
-          <div className={styles.countdown}>
-            <h3 className={styles.countdownText}>
-              {formatHoursMinutes(timeLeft)}:
-              <span className={styles.seconds}>{seconds}</span>
-            </h3>
-          </div>
-          <div className={styles.price}>
-            <span className={styles.discountedPrice}>
-              {item.discountedPrice}원
-            </span>
-            <span className={styles.originalPrice}>{item.originalPrice}원</span>
-          </div>
-          <div className={styles.details}>
-            <p>{item.description}</p>
-            <div className={styles.rating}>
-              {[...Array(item.rating)].map((_, index) => (
-                <FaStar key={index} className={styles.star} />
-              ))}
-              <span>{item.reviews}</span>
+    return (
+        <div className={styles.timeItem}>
+            <div className={styles.body}>
+                <div className={styles.image}>
+                    <img src={item.image} alt='Product'/>
+                </div>
+                <div className={styles.info}>
+                    <div className={styles.countdown}>
+                        <h3 className={styles.countdownText}>
+                            {formatHoursMinutes(timeLeft)}:
+                            <span className={styles.seconds}>{seconds}</span>
+                        </h3>
+                    </div>
+                    <div className={styles.description}>{item.description}</div>
+                    <div className={styles.details}>
+                        <div className={styles.price}>
+                            <span className={styles.priceTitle}>타임딜가</span>
+                            <span className={styles.discountedPrice}>
+                                {item.discountedPrice}원
+                            </span>
+                            <span className={styles.originalPrice}>{item.originalPrice}원</span>
+                        </div>
+                        <div className={styles.rating}>
+                            {[...Array(5)].map((_, index) => (
+                                <FaStar
+                                    key={index}
+                                    className={index < item.rating ? styles.star : styles.grayStar}
+                                />
+                            ))}
+                            <span className={styles.reviews}>({item.reviews})</span>
+                        </div>
+                        <p>{item.details}</p>
+                        <div className={styles.coupon}>
+                            <span>{item.coupon}</span> {item.couponDescription}
+                        </div>
+                    </div>
+                </div>
             </div>
-            <p>{item.details}</p>
-            <div className={styles.coupon}>
-              <span>{item.coupon}</span> {item.couponDescription}
-            </div>
-          </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default TimeItem;
--- frontend/src/pages/category/TimeDeal.tsx
+++ frontend/src/pages/category/TimeDeal.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, {useEffect} from 'react';
 import styles from './TimeDeal.module.css';
 import Header from '../../layout/Header/Header';
 import Fixedheader from '../../layout/Header/Fixedheader';
@@ -7,30 +7,30 @@ import TimeItem from '../../components/timedeal/TimeItem';
 import TimeItemList from '../../components/timedeal/TimeItemList';
 import Footer from '../../components/home/Footer';
 
-const TimeDeal = ({ category }: any) => {
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, [category]); // Dependency array with 'category' to re-trigger when category changes
+const TimeDeal = ({category}: any) => {
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, [category]); // Dependency array with 'category' to re-trigger when category changes
 
-  // 무한 스크롤 기능 넣기
+    // 무한 스크롤 기능 넣기
 
-  return (
-    <div>
-      <Fixedheader />
-      <Header />
-      <Productcategory />
+    return (
+        <div>
+            <Fixedheader/>
+            <Header/>
+            <Productcategory/>
 
-      <div className={styles.container}>
-        <div className={styles.inner}>
-          {/* 아이템 리스트에 아이템 정보 넣기 */}
-          <span className={styles.title}>타임딜</span>
-          <TimeItemList />
+            <div className={styles.container}>
+                <div className={styles.inner}>
+                    {/* 아이템 리스트에 아이템 정보 넣기 */}
+                    <span className={styles.title}>지금 당장 구매하세요 안사면 후회하는 가격!</span>
+                    <TimeItemList/>
+                </div>
+            </div>
+
+            <Footer/>
         </div>
-      </div>
-
-      <Footer />
-    </div>
-  );
+    );
 };
 
 export default TimeDeal;
