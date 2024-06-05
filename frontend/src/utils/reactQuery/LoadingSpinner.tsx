14fbcf1c370feb2ebdc7bc2e639ceea720d99084
pointtt <50075024+gksktl111@users.noreply.github.com>
2024-06-05 15:38:10 +0900

    feat: 쿼리 로딩창 에러창 개발

---
+++ frontend/src/utils/reactQuery/LoadingSpinner.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import styles from './LoadingSpinner.module.css';
+import Fixedheader from "../../layout/Header/Fixedheader";
+import Header from "../../layout/Header/Header";
+
+const LoadingSpinner: React.FC = () => {
+    return (
+        <div>
+            <Header/>
+            <div className={styles.spinnerContainer}>
+                <div className={styles.spinner}></div>
+            </div>
+        </div>
+    );
+};
+
+export default LoadingSpinner;
