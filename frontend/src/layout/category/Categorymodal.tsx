60b8595851a0b09ed85e02ddd4b2a23ba7a23dca
pointtt <50075024+gksktl111@users.noreply.github.com>
2024-06-05 14:31:25 +0900

    feat: 헤더 css 수정

--- frontend/src/layout/category/Categorymodal.tsx
+++ frontend/src/layout/category/Categorymodal.tsx
@@ -1,118 +1,130 @@
-import React, { useEffect, useState } from 'react';
-import { SidebarProps } from '../../typings/layout';
+import React, {useEffect, useState} from 'react';
+import {SidebarProps} from '../../typings/layout';
 import styles from './Categorymodal.module.css';
-import { Link } from 'react-router-dom';
+import {Link} from 'react-router-dom';
 
 const categories = [
-  {
-    name: '강아지',
-    link: '/dog',
-    subCategories: [
-      '카테고리7',
-      '카테고리8',
-      '카테고리9',
-      '카테고리22',
-      '카테고리23',
-      '카테고리24',
-      '카테고리25',
-      '카테고리26',
-    ],
-  },
-  {
-    name: '고양이',
-    link: '/cat',
-    subCategories: [
-      '카테고리10',
-      '카테고리11',
-      '카테고리12',
-      '카테고리27',
-      '카테고리28',
-      '카테고리29',
-    ],
-  },
-  {
-    name: '설치류',
-    link: '/rat',
-    subCategories: [
-      '카테고리13',
-      '카테고리14',
-      '카테고리15',
-      '카테고리32',
-      '카테고리33',
-      '카테고리34',
-      '카테고리35',
-      '카테고리36',
-      '카테고리34',
-    ],
-  },
-  {
-    name: '조류',
-    link: '/bird',
-    subCategories: ['카테고리16', '카테고리17'],
-  },
-  {
-    name: '파충류',
-    link: '/reptiles',
-    subCategories: ['카테고리19', '카테고리20', '카테고리21', '카테고리42'],
-  },
+    {
+        name: '강아지',
+        link: '/dog',
+        subCategories: [
+            '강아지 사료',
+            '강아지 장난감',
+            '강아지 집',
+            '강아지 목줄 및 목걸이',
+            '강아지 의류',
+            '강아지 미용',
+            '강아지 건강',
+        ],
+    },
+    {
+        name: '고양이',
+        link: '/cat',
+        subCategories: [
+            '고양이 사료',
+            '고양이 장난감',
+            '고양이 집',
+            '고양이 옷',
+            '고양이 미용',
+            '고양이 건강',
+            '고양이 화장실',
+            '고양이 나무와 가구',
+            '캣잎',
+        ],
+    },
+    {
+        name: '설치류',
+        link: '/rat',
+        subCategories: [
+            '설치류 먹이',
+            '설치류 장난감',
+            '설치류 집',
+            '설치류 옷',
+            '설치류 미용',
+
+        ],
+    },
+    {
+        name: '조류',
+        link: '/bird',
+        subCategories: [
+            '새 먹이',
+            '새 장난감',
+            '새 집',
+            '새 미용',
+            '새 건강',
+            '새 케이지'],
+    },
+    {
+        name: '파충류',
+        link: '/reptiles',
+        subCategories: [
+            '파충류 먹이',
+            '파충류 장난감',
+            '파충류 미용',
+            '파충류 건강',
+            '파충류 케이지',
+            '파충류 히터 및 조명',
+        ],
+    },
 ];
 
-const Categorymodal = ({ isOpen, setOpen, setClose }: SidebarProps) => {
-  const [scrollY, setScrollY] = useState(0);
+const Categorymodal = ({isOpen, setOpen, setClose}: SidebarProps) => {
+    const [scrollY, setScrollY] = useState(0);
 
-  useEffect(() => {
-    const handleScroll = () => {
-      setScrollY(window.scrollY); // 스크롤 Y 값을 업데이트
+    useEffect(() => {
+        const handleScroll = () => {
+            setScrollY(window.scrollY); // 스크롤 Y 값을 업데이트
+        };
+
+        window.addEventListener('scroll', handleScroll);
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
+    // 마우스가 모달 영역을 벗어날 때 모달 열림 상태를 false로 설정
+    const handleMouseLeave = () => {
+        setClose();
     };
 
-    window.addEventListener('scroll', handleScroll);
-
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, []);
-
-  // 마우스가 모달 영역을 벗어날 때 모달 열림 상태를 false로 설정
-  const handleMouseLeave = () => {
-    setClose();
-  };
-
-  return (
-    <div
-      className={isOpen ? styles.modal__open : styles.modal__close}
-      onMouseLeave={handleMouseLeave} // 모달 영역을 벗어날 때 호출
-    >
-      <div
-        className={scrollY >= 200 ? styles.modal__fixed : styles.modal__content}
-      >
-        {isOpen && (
-          <div className={styles.categories__container}>
-            {categories.map((category, index) => (
-              <div key={index} className={styles.category}>
-                <div className={styles.category__content}>
-                  <Link to={category.link} className={styles.category__link}>
+    return (
+        <div
+            className={isOpen ? styles.modal__open : styles.modal__close}
+            onMouseLeave={handleMouseLeave} // 모달 영역을 벗어날 때 호출
+        >
+            <div
+                className={scrollY >= 200 ? styles.modal__fixed : styles.modal__content}
+            >
+                {isOpen && (
+                    <div className={styles.categories__container}>
+                        {categories.map((category, index) => (
+                            <div key={index} className={styles.category}>
+                                <div className={styles.category__content}>
+                                    <Link to={category.link} className={styles.category__link}>
                     <span className={styles.category__title}>
                       {category.name}
                     </span>
-                  </Link>
+                                    </Link>
 
-                  <div className={styles.ul__wrap}>
-                    <div className={styles.ul__line__bg}></div>
-                    <div className={styles.ul__line}></div>
-                    <ul>
-                      {category.subCategories.map((subCategory, subIndex) => (
-                        <li key={subIndex}>{subCategory}</li>
-                      ))}
-                    </ul>
-                  </div>
-                </div>
-              </div>
-            ))}
-          </div>
-        )}
-      </div>
-    </div>
-  );
+                                    <div className={styles.ul__wrap}>
+                                        <div className={styles.ul__line__bg}></div>
+                                        <div className={styles.ul__line}></div>
+                                        <ul>
+                                            {category.subCategories.map((subCategory, subIndex) => (
+                                                <li key={subIndex}>{subCategory}</li>
+                                            ))}
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                )}
+            </div>
+        </div>
+    );
 };
 
 export default Categorymodal;
