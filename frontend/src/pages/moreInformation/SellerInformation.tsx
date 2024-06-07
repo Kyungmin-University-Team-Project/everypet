81e5c4af50332b5cd55e3e799f6596fa19bc19cd
(Leekwanyong) <(dlrkdid12@gmail.com)>
2024-06-07 19:28:40 +0900

    feat: ADD 판매자 정보에 대한 UI 컴포넌트 화

--- frontend/src/pages/moreInformation/SellerInformation.tsx
+++ frontend/src/pages/moreInformation/SellerInformation.tsx
@@ -1,9 +1,47 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import ShippingInformation from '../../components/moreinformation/ShippiongInformation';
 
-const SellerInformation = () => {
+interface DeliveryInformation {
+    "Delivery method": string;
+    "Bundle delivery availability": string;
+    "Shipping cost": string;
+    "Delivery Period": string[];
+}
+
+interface ExchangeReturnInformation {
+    "General Guidelines": string;
+    "Exchange/Return Cost": {
+        "Wow Membership Member": string;
+        "Non-members": string[];
+    };
+    "Reference date for exchange/return application": string[];
+    "Exchange/Return Restrictions": string[];
+}
+
+interface SellerInfo {
+    "Shipping Information": DeliveryInformation;
+    "Exchange/Return Information": ExchangeReturnInformation;
+}
+
+const SellerInformation: React.FC = () => {
+    const [sellerInfo, setSellerInfo] = useState<SellerInfo | null>(null);
+
+    useEffect(() => {
+        fetch('/mock/shipping_information.json')
+            .then((response) => response.json())
+            .then((data) => setSellerInfo(data));
+    }, []);
+
+    if (!sellerInfo) {
+        return <div>Loading...</div>;
+    }
+
+    const deliveryInfo = sellerInfo["Shipping Information"];
+    const returnInfo = sellerInfo["Exchange/Return Information"];
+
     return (
         <div>
-            Seller
+            <ShippingInformation deliveryInfo={deliveryInfo} returnInfo={returnInfo} />
         </div>
     );
 };
