daeb5765aafc23e090e69ed90b4c5ad1bf1c7817
pointtt <50075024+gksktl111@users.noreply.github.com>
2024-08-06 12:24:07 +0900

    feat: 상세 카테고리 로딩 오류 수정

--- frontend/src/pages/category/ProductPage.tsx
+++ frontend/src/pages/category/ProductPage.tsx
@@ -1,8 +1,9 @@
-import React, { useEffect, useState } from 'react';
+import React, {useEffect, useState} from 'react';
 import styles from './ProductPage.module.css';
 
 import ItemList from '../../components/common/ItemList';
 import DetailedCategory from '../../components/common/DetailedCategory';
+import LoadingSpinner from "../../utils/reactQuery/LoadingSpinner";
 
 interface DetailedCategory {
     id: number;
@@ -16,21 +17,24 @@ const fetchCategoryDetails = async (category: string): Promise<{ detailedCategor
     return response.json();
 };
 
-const ProductPage = ({ category }: { category: string }) => {
+const ProductPage = ({category}: { category: string }) => {
     const [data, setData] = useState<{ detailedCategories: DetailedCategory[] } | null>(null);
+    const [isLoading, setIsLoading] = useState(true);  // 로딩 상태 추가
 
     useEffect(() => {
         const getCategoryDetails = async () => {
+            setIsLoading(true);  // 데이터 로딩 시작
             try {
                 const result = await fetchCategoryDetails(category);
                 setData(result);
             } catch (error) {
                 console.error('Failed to fetch category details:', error);
+            } finally {
+                setIsLoading(false);  // 데이터 로딩 완료
             }
         };
 
         // 비동기 함수를 즉시 실행
-        // 프로미스 반환 경고 때문에 즉시 실행 시킴
         (async () => {
             await getCategoryDetails();
         })();
@@ -38,14 +42,20 @@ const ProductPage = ({ category }: { category: string }) => {
 
     useEffect(() => {
         window.scrollTo(0, 0);
-    }, [category]); // Dependency array with 'category' to re-trigger when category changes
+    }, [category]);
 
     return (
         <div>
             <div className={styles.container}>
                 <div className={styles.inner}>
-                    {data && <DetailedCategory details={data.detailedCategories} />}
-                    <ItemList />
+                    {isLoading ? (
+                        <LoadingSpinner/>
+                    ) : (
+                        <>
+                            {data && <DetailedCategory details={data.detailedCategories}/>}
+                            <ItemList/>
+                        </>
+                    )}
                 </div>
             </div>
         </div>
