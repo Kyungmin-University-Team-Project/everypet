4883c5136d7e4af303871b29396bcd7cb1af0965
pointtt <50075024+gksktl111@users.noreply.github.com>
2024-08-07 11:17:09 +0900

    feat: quick 배너 카테고리 연결

--- frontend/src/components/home/quicknav/Quicknav.tsx
+++ frontend/src/components/home/quicknav/Quicknav.tsx
@@ -1,45 +1,56 @@
 import React from 'react';
+import {useNavigate} from 'react-router-dom';
+import {useDispatch} from 'react-redux';
 import styles from './Quicknav.module.css';
+import {setClickedCategory} from '../../../redux/features/categorySlice';
 
-const categories = [
-  {
-    name: '타임딜',
-    image: require('../../../assets/img/quicknav/imminent.png'),
-  },
-  { name: '신상품', image: require('../../../assets/img/quicknav/new.png') },
-  { name: '강아지', image: require('../../../assets/img/quicknav/dog.png') },
-  { name: '고양이', image: require('../../../assets/img/quicknav/cat.png') },
-  {
-    name: '설치류',
-    image: require('../../../assets/img/quicknav/hamster.png'),
-  },
-  {
-    name: '조류',
-    image: require('../../../assets/img/quicknav/bird.png'),
-  },
-  {
-    name: '파충류',
-    image: require('../../../assets/img/quicknav/retail.png'),
-  },
+interface Category {
+    name: string;
+    image: string;
+    link: string;
+}
+
+const categories: Category[] = [
+    {name: '기획전', image: require('../../../assets/img/quicknav/Exhibitions.png'), link: '/exhibitions'},
+    {name: '타임딜', image: require('../../../assets/img/quicknav/imminent.png'), link: '/timeDeal'},
+    {name: '강아지', image: require('../../../assets/img/quicknav/dog.png'), link: '/dog'},
+    {name: '고양이', image: require('../../../assets/img/quicknav/cat.png'), link: '/cat'},
+    {name: '설치류', image: require('../../../assets/img/quicknav/hamster.png'), link: '/rat'},
+    {name: '조류', image: require('../../../assets/img/quicknav/bird.png'), link: '/bird'},
+    {name: '파충류', image: require('../../../assets/img/quicknav/retail.png'), link: '/reptiles'},
 ];
 
-const Quicknav = () => {
-  return (
-    <div className={styles.quick__nav__container}>
-      <nav className={styles.quick__nav}>
-        {categories.map((category) => (
-          <div key={category.name} className={styles.quick__nav__item}>
-            <img
-              className={styles.img}
-              src={category.image}
-              alt={category.name}
-            />
-            <span>{category.name}</span>
-          </div>
-        ))}
-      </nav>
-    </div>
-  );
+const Quicknav: React.FC = () => {
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    const handleClick = (category: Category) => {
+        dispatch(setClickedCategory(category.name));
+        navigate(category.link);
+    };
+
+    return (
+        <div className={styles.quick__nav__container}>
+            <nav className={styles.quick__nav}>
+                {categories.map((category) => (
+                    <div
+                        key={category.name}
+                        className={styles.quick__nav__item}
+                        onClick={() => handleClick(category)}
+                    >
+                        <div className={styles.img__container}>
+                            <img
+                                className={styles.img}
+                                src={category.image}
+                                alt={category.name}
+                            />
+                        </div>
+                        <span>{category.name}</span>
+                    </div>
+                ))}
+            </nav>
+        </div>
+    );
 };
 
 export default Quicknav;
