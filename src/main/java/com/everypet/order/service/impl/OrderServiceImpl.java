1f9490ada69ec6d75920c4c26348e4018c55993f
noheechul <noheechul0511@naver.com>
2024-08-14 01:57:40 +0900

    feat: 주문 저장 받는 데이터 수정

--- src/main/java/com/everypet/order/service/impl/OrderServiceImpl.java
+++ src/main/java/com/everypet/order/service/impl/OrderServiceImpl.java
@@ -8,6 +8,7 @@ import com.everypet.order.service.OrderService;
 import com.everypet.product.model.dao.ProductMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -21,34 +22,60 @@ public class OrderServiceImpl implements OrderService {
     private final OrderDetailMapper orderDetailMapper;
 
     @Override
+    @Transactional
     public void insertOrder(InsertOrderDTO insertOrderDTO, String memberId) {
 
         int totalAmount = 0;
+        try {
+            // 주문 상품의 가격을 조회하고, 총 금액을 계산한다.
+            for (InsertOrderDTO.Products product : insertOrderDTO.getProducts()){
+                int discountedProductPrice = productMapper.selectDiscountedProductPriceByProductId(product.getProductId());
+                totalAmount += discountedProductPrice * product.getQuantity();
+            }
+            totalAmount += insertOrderDTO.getDelivery(); // 배송비 추가
 
-        // 주문 상품의 가격을 조회하고, 총 금액을 계산한다.
-        for (InsertOrderDTO.Products product : insertOrderDTO.getProducts()){
-            int discountedProductPrice = productMapper.selectDiscountedProductPriceByProductId(product.getProductId());
-            totalAmount += discountedProductPrice * product.getQuantity();
+            // 주문 정보를 저장한다.
+            Map<String, Object> map = new HashMap<>();
+
+            map.put("orderId", insertOrderDTO.getOrderId());
+            map.put("memberId", memberId);
+            map.put("totalAmount", totalAmount);
+            map.put("productAmount", totalAmount - insertOrderDTO.getDelivery());
+            map.put("deliveryAmount", insertOrderDTO.getDelivery());
+
+            // 배송 정보 추가
+            map.put("postalCode", insertOrderDTO.getPostalCode());
+            map.put("address", insertOrderDTO.getAddress());
+            map.put("addressDetail", insertOrderDTO.getAddressDetail());
+            map.put("receiver", insertOrderDTO.getReceiver());
+            map.put("phone", insertOrderDTO.getPhone());
+            map.put("request", insertOrderDTO.getRequest());
+
+            int insertOrderResult = orderMapper.insertOrder(map);
+
+            if (insertOrderResult == 0) {
+                throw new RuntimeException("주문 등록에 실패했습니다.");
+            }
+        }catch (Exception e) {
+            throw new RuntimeException("주문 등록에 실패했습니다.");
         }
 
-        // 주문 정보를 저장한다.
-        Map<String, Object> map = new HashMap<>();
-        map.put("orderId", insertOrderDTO.getOrderId());
-        map.put("memberId", memberId);
-        map.put("totalAmount", totalAmount);
-        map.put("addressId", insertOrderDTO.getAddressId());
 
-        orderMapper.insertOrder(map);
+        try {
+            // 주문 디테일 정보를 저장한다.
+            for (InsertOrderDTO.Products product : insertOrderDTO.getProducts()){
+                Map<String, Object> orderDetailMap = new HashMap<>();
+                orderDetailMap.put("orderId", insertOrderDTO.getOrderId());
+                orderDetailMap.put("productId", product.getProductId());
+                orderDetailMap.put("quantity", product.getQuantity());
 
-        // 주문 디테일 정보를 저장한다.
-        for (InsertOrderDTO.Products product : insertOrderDTO.getProducts()){
-            Map<String, Object> orderDetailMap = new HashMap<>();
-            orderDetailMap.put("orderId", insertOrderDTO.getOrderId());
-            orderDetailMap.put("productId", product.getProductId());
-            orderDetailMap.put("quantity", product.getQuantity());
-
-            orderDetailMapper.insertOrderDetail(orderDetailMap);
+                orderDetailMapper.insertOrderDetail(orderDetailMap);
+            }
+        }catch (Exception e) {
+            System.out.println("주문 디테일 등록에 실패했습니다.");
+            throw new RuntimeException("주문 디테일 등록에 실패했습니다.");
         }
+
     }
 
     @Override
@@ -74,7 +101,7 @@ public class OrderServiceImpl implements OrderService {
 
             Map<String, Object> map = new HashMap<>();
             map.put("orderId", orderId);
-            map.put("status", status);
+            map.put("orderStatus", status);
 
             orderMapper.updateOrderStatusByOrderId(map);
     }
