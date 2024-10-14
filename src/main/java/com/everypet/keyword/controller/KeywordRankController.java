481867c8ba25db13c58be96ccfc4419a3a183640
yongho9064 <dydgmldydgh@naver.com>
2024-10-14 20:25:25 +0900

    refactor: 실시간 검색어 순위 날짜 제거

--- src/main/java/com/everypet/keyword/controller/KeywordRankController.java
+++ src/main/java/com/everypet/keyword/controller/KeywordRankController.java
@@ -1,19 +1,18 @@
 package com.everypet.keyword.controller;
 
-import com.everypet.global.util.ResponseEntityUtil;
 import com.everypet.keyword.model.dto.KeywordRankDTO.TopKeywordRank;
 import com.everypet.keyword.service.KeywordRankService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 
-@Api("랭크 Api")
+@Api(tags = "키워드 랭킹 Api")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/keyword-rank")
@@ -24,8 +23,8 @@ public class KeywordRankController {
     // 실시간 검색어 순위
     @ApiOperation(value = "실시간 검색어 순위", notes = "실시간 검색어 순위와 변동 상태")
     @PostMapping("/real-time-rank")
-    public ResponseEntity<TopKeywordRank> realTimeKeyword() {
-        return ResponseEntityUtil.response(keywordRankService.findAllTopKeywordRank(), HttpStatus.OK);
+    public ResponseEntity<List<TopKeywordRank>> realTimeKeyword() {
+        return ResponseEntity.ok(keywordRankService.findAllTopKeywordRank());
     }
 
 }
--- src/main/java/com/everypet/keyword/model/dao/KeywordRankMapper.java
+++ src/main/java/com/everypet/keyword/model/dao/KeywordRankMapper.java
@@ -1,7 +1,7 @@
 package com.everypet.keyword.model.dao;
 
 import com.everypet.keyword.model.dto.KeywordRankDTO;
-import com.everypet.keyword.model.dto.KeywordRankDTO.TopKeywordRankDetail;
+import com.everypet.keyword.model.dto.KeywordRankDTO.TopKeywordRank;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
@@ -12,7 +12,7 @@ public interface KeywordRankMapper {
     // 키워드 랭킹을 모두 조회
     List<KeywordRankDTO> findAllKeywordRank();
     // 1~10위까지의 키워드 랭킹을 조회
-    List<TopKeywordRankDetail> findTopKeywordRankDetail();
+    List<TopKeywordRank> findTopKeywordRank();
     void saveKeywordRank(KeywordRankDTO keyword);
     void updateTotalScore(KeywordRankDTO keyword);
     KeywordRankDTO findKeywordRank(String keyword);
