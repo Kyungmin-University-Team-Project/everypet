21d36d202573053df5ff323fbcea03a1e9ede42d
yongho9064 <dydgmldydgh@naver.com>
2024-10-14 20:27:13 +0900

    feat: KeywordLog 클래스에 @JsonFormat 추가하여 JSON 날짜 형식 개선

--- src/main/java/com/everypet/keyword/controller/KeywordLogController.java
+++ src/main/java/com/everypet/keyword/controller/KeywordLogController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Api("키워드 로그 Api")
+@Api(tags = "키워드 로그 Api")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/keyword-log")
