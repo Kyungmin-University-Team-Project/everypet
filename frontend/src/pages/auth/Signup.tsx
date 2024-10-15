c7d1ab93c4a900fb4dc42a5401c940e0e9d2e3d2
Leekwanyong <dlrkdid12@gmail.com>
2024-10-15 15:40:31 +0900

    chore: 회원가입 버그 수정

--- frontend/src/pages/auth/Signup.tsx
+++ frontend/src/pages/auth/Signup.tsx
@@ -1,11 +1,21 @@
-import React, { useState } from "react";
-import { useLocation, useNavigate } from "react-router-dom";
+import React, {FormEvent, useState} from "react";
+import {useLocation, useNavigate} from "react-router-dom";
 import Postcode from "./Postcode";
 import styles from "./Signup.module.css";
-import { signUpLogin, sendVerificationEmail, verifyCode } from "../../utils/auth/AuthAPI";
+
 import "@fortawesome/fontawesome-free/css/all.css";
 import axios from "axios";
-import { Join } from "../../typings/signup";
+import {Code, Join} from "../../typings/signup";
+import {sendVerificationEmail, signUpLogin, verifyCode} from "../../utils/auth/AuthAPI";
+
+const validateInput = (value: string, allowKorean: boolean, allowEnglish: boolean) => {
+    const hasKorean = /[ㄱ-ㅎ|ㅏ-ㅣ가-힣]/.test(value);
+    const hasEnglish = /[A-Za-z]/.test(value);
+    // 조건에 따른 필터링
+    if (!allowKorean && hasKorean) return false;
+    return !(!allowEnglish && hasEnglish);
+
+}
 
 const Signup: React.FC = () => {
     const location = useLocation();
@@ -26,23 +36,16 @@ const Signup: React.FC = () => {
         },
         agreeMarketingYn: agreeMarketingYn,
     });
-
+    const [code, setCode] = useState<Code>({code: ''});
     const [disableButton, setDisableButton] = useState(true);
     const [emailSent, setEmailSent] = useState(false);
     const [emailVerified, setEmailVerified] = useState(false);
 
-
     const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
         try {
-            console.log("Sending user data:", JSON.stringify(user, null, 2));
-
-            const response = await signUpLogin(user);
-
-            console.log("Server response:", response);
-            if (response.success) {
-                navigate('/'); // 가입 성공 후 리다이렉션
-            }
+            await signUpLogin(user);
+            navigate('/')
         } catch (error) {
             if (axios.isAxiosError(error) && error.response) {
                 console.log("Error response data:", error.response.data);
@@ -53,62 +56,73 @@ const Signup: React.FC = () => {
             }
         }
     };
-
-    const hi = (e: React.FocusEvent<HTMLInputElement>) => {
-        const inputValue = e.target.value.trim();
-
-        if (!inputValue || user.memberId.length < 5 || user.memberPwd.length < 5) {
-            e.target.placeholder = "아이디를 5글자 이상 입력해주세요.";
-            e.target.style.border = "1px solid rgb(231, 62, 62)";
-            setDisableButton(true);
-        } else {
-            setDisableButton(false);
-            e.target.style.border = "1px solid #888";
-        }
-    };
-
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        const { name, value } = e.target;
-        if (name === "address" || name === "detailAddress") {
-            setUser((prevUser) => ({
-                ...prevUser,
-                address: {
-                    ...prevUser.address,
-                    [name]: value
+        const {name, value} = e.target;
+
+        let allowKorean = true;
+        let allowEnglish = true;
+        // 필드별 검증 규칙 설정
+        if (name === "referrer" || name === "email" || name === "code") allowKorean = false;
+        if (name === 'memberId') allowKorean = false;
+        // 입력값 검증
+        if (validateInput(value, allowKorean, allowEnglish)) {
+            setUser((prevUser) => {
+                // 주소 관련 필드만 업데이트
+                if (name === "address" || name === "detailAddress") {
+                    return {
+                        ...prevUser,
+                        address: {
+                            ...prevUser.address,
+                            [name]: value,
+                        },
+                    };
+                } else {
+                    // 다른 필드 업데이트
+                    return {
+                        ...prevUser,
+                        [name]: value,
+                    };
                 }
-            }));
-        } else {
-            setUser({ ...user, [name]: value });
+            });
+        }
+    }
+
+    const handleMaxLength = (e: FormEvent<HTMLInputElement>, maxLength: number) => {
+        const inputElement = e.target as HTMLInputElement;
+        const {value} = inputElement;
+        if (value.length > maxLength) {
+            inputElement.value = value.substr(0, maxLength);
         }
     };
+
+    const handleCodeChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const {name, value} = e.target as HTMLInputElement;
+        const isKoreanOrEnglish = /[ㄱ-ㅎ|ㅏ-ㅣ|가-힣|A-Za-z]/.test(value);
+        if (!isKoreanOrEnglish) setCode({...code, [name]: value});
+    }
 
     const handleAddressChange = (address: string, detailAddress: string) => {
-        setUser({ ...user, address: { address, detailAddress } });
+        setUser({...user, address: {address, detailAddress}});
     };
 
     const handleSendVerificationEmail = async () => {
         try {
-            const response = await sendVerificationEmail({  purpose: 'SIGNUP', to: user.email });
-            console.log("Verification email sent:", response);
-            setEmailSent(true); // Set state to indicate email has been sent
+            await sendVerificationEmail({purpose: 'SIGNUP', to: user.email.trim()});
+            setEmailSent(true);
         } catch (error) {
             if (axios.isAxiosError(error) && error.response) {
                 console.log("Error response data:", error.response.data);
-                console.log("Error response status:", error.response.status);
-                console.log("Error response headers:", error.response.headers);
             } else {
                 console.log("Error sending verification email:", error);
             }
         }
     };
 
-    const [verificationCode, setVerificationCode] = useState("");
-
     const handleVerifyCode = async () => {
         try {
-            const response = await verifyCode({ email: user.email, code: verificationCode });
-            console.log("Verification successful:", response);
-            setEmailVerified(true); // Set state to indicate email verification successful
+            await verifyCode({purpose: 'SIGNUP', code: code.code});
+            console.log()
+            setEmailVerified(true);
         } catch (error) {
             if (axios.isAxiosError(error) && error.response) {
                 console.log("Error response data:", error.response.data);
@@ -131,10 +145,12 @@ const Signup: React.FC = () => {
                     name="memberId"
                     id="memberId"
                     onChange={handleChange}
-                    placeholder="아이디"
+                    placeholder="아이디 영어, 숫자, 특수문자만 입력이 가능해요!"
                     className={styles.input_value}
-                    onBlur={hi}
                     required
+                    onInput={(e) => {
+                        handleMaxLength(e, 15)
+                    }}
                 />
             </label>
             <label htmlFor="memberPwd" className={styles.label_container}>
@@ -143,10 +159,12 @@ const Signup: React.FC = () => {
                     value={user.memberPwd}
                     name="memberPwd"
                     id="memberPwd"
+                    onInput={(e) => {
+                        handleMaxLength(e, 15)
+                    }}
                     onChange={handleChange}
                     placeholder="비밀번호"
                     className={styles.input_value}
-                    onBlur={hi}
                     required
                 />
             </label>
@@ -160,6 +178,9 @@ const Signup: React.FC = () => {
                     className={styles.input_value}
                     value={user.email}
                     onChange={handleChange}
+                    onInput={(e) => {
+                        handleMaxLength(e, 35)
+                    }}
                     required
                 />
                 <button
@@ -173,18 +194,21 @@ const Signup: React.FC = () => {
                 {emailSent && (
                     <>
                         <input
-                            type="text"
-                            value={verificationCode}
-                            onChange={(e) => setVerificationCode(e.target.value)}
-                            placeholder="인증 코드"
+                            name='code'
+                            id='code'
+                            onChange={handleCodeChange}
+                            value={code.code}
+                            placeholder="인증 코드 숫자만 입력이 가능해요!"
                             className={styles.input_value}
+                            onInput={(e) => {
+                                handleMaxLength(e, 4)
+                            }}
                             required
                         />
                         <button
                             type="button"
                             onClick={handleVerifyCode}
                             className={styles.verify_btn}
-                            disabled={!verificationCode}
                         >
                             인증 코드 확인
                         </button>
@@ -200,18 +224,25 @@ const Signup: React.FC = () => {
                     onChange={handleChange}
                     placeholder="이름"
                     className={styles.input_value}
+                    onInput={(e) => {
+                        handleMaxLength(e, 25)
+                    }}
                     required
                 />
             </label>
 
             <label className={styles.label_container}>
                 <input
+                    type='date'
                     id="birth"
                     name="birth"
                     value={user.birth}
                     onChange={handleChange}
-                    placeholder="생년월일 8자리"
+                    placeholder="생년월일 입력 해주세요."
                     className={styles.input_value}
+                    onInput={(e) => {
+                        handleMaxLength(e, 10)
+                    }}
                     required
                 />
             </label>
@@ -221,13 +252,16 @@ const Signup: React.FC = () => {
                     name="phone"
                     value={user.phone}
                     onChange={handleChange}
-                    placeholder="핸드폰 번호(-)금지"
+                    placeholder="핸드폰 번호"
                     className={styles.input_value}
+                    onInput={(e) => {
+                        handleMaxLength(e, 15)
+                    }}
                     required
                 />
             </label>
 
-            <Postcode onAddressChange={handleAddressChange} />
+            <Postcode onAddressChange={handleAddressChange}/>
 
             <label className={styles.label_container}>
                 <input
@@ -235,11 +269,14 @@ const Signup: React.FC = () => {
                     name="referrer"
                     value={user.referrer}
                     onChange={handleChange}
-                    placeholder="추천인"
+                    placeholder="추천인 영어만 입력이 가능해요!"
+                    onInput={(e) => {
+                        handleMaxLength(e, 10)
+                    }}
                     className={styles.input_value}
                 />
             </label>
-            <button type="submit" disabled={disableButton || !emailVerified} className={styles.join_btn}>
+            <button type="submit" disabled={disableButton && !emailVerified} className={styles.join_btn}>
                 가입하기
             </button>
         </form>
--- frontend/src/utils/auth/AuthAPI.ts
+++ frontend/src/utils/auth/AuthAPI.ts
@@ -1,15 +1,15 @@
-import axios, { AxiosError, AxiosRequestConfig, AxiosResponse } from 'axios';
-import { Join } from "../../typings/signup";
-import { LoginData } from "../../typings/login";
+import axios, {AxiosError, AxiosRequestConfig, AxiosResponse} from 'axios';
+import {Join} from "../../typings/signup";
+import {LoginData} from "../../typings/login";
 // jwt 4.0 이상
-import { jwtDecode } from 'jwt-decode';
+import {jwtDecode} from 'jwt-decode';
 
 export interface ResponseData {
     access: string;
     user: string;
 }
 
-const api = axios.create({ baseURL: '/' });
+const api = axios.create({baseURL: 'http://localhost:8080'});
 
 // 401 에러 처리
 api.interceptors.request.use(
@@ -20,15 +20,15 @@ api.interceptors.request.use(
         if (error.response && error.response.status === 401) {
             try {
                 let errorConfig: AxiosRequestConfig = error.config || {}; // errorConfig가 undefined인 경우 빈 객체로 설정
-                const { data } = await axios.post('access/refresh'); // axios로 요청 보내기
+                const {data} = await axios.post('access/refresh'); // axios로 요청 보내기
                 if (data) {
-                    const { access } = data;
+                    const {access} = data;
                     localStorage.removeItem('access');
                     localStorage.setItem('access', access);
                     if (errorConfig.headers) {
                         errorConfig.headers['Authorization'] = `Bearer ${access}`; // 새로운 토큰으로 헤더 업데이트
                     } else {
-                        errorConfig.headers = { Authorization: `Bearer ${access}` }; // 헤더가 없는 경우 새로 생성
+                        errorConfig.headers = {Authorization: `Bearer ${access}`}; // 헤더가 없는 경우 새로 생성
                     }
                     return axios.request(errorConfig); // 기존 요청 재시도
                 }
@@ -42,8 +42,8 @@ api.interceptors.request.use(
     }
 );
 
-export const login = async ({ memberId, memberPwd }: LoginData): Promise<any> => {
-    const data = { memberId, memberPwd };
+export const login = async ({memberId, memberPwd}: LoginData): Promise<any> => {
+    const data = {memberId, memberPwd};
 
     console.log("Payload sent to server:", JSON.stringify(data, null, 2));
     try {
@@ -79,7 +79,7 @@ export const login = async ({ memberId, memberPwd }: LoginData): Promise<any> =>
 export const signUpLogin = async (user: Join): Promise<any> => {
     console.log("Payload sent to server:", JSON.stringify(user, null, 2));
     try {
-        const response: AxiosResponse<any> = await api.post('/signup', user);
+        const response: AxiosResponse<any> = await api.post('/member/signup', user);
         console.log("Server response:", response.data);
         return response.data;
     } catch (error) {
@@ -94,9 +94,11 @@ export const signUpLogin = async (user: Join): Promise<any> => {
     }
 };
 
-export const sendVerificationEmail = async ({  purpose, to }: {  purpose:string; to: string }): Promise<{ success: boolean }> => {
-    const data = {  purpose, to };
-    const response = await api.post('http://localhost:8080/send-mail/code', data, {
+export const sendVerificationEmail = async ({purpose, to}: { purpose: string; to: string }): Promise<{
+    success: boolean
+}> => {
+    const data = {purpose, to};
+    const response = await api.post('/send-mail/code', data, {
         headers: {
             'Content-Type': 'application/json'
         }
@@ -105,13 +107,16 @@ export const sendVerificationEmail = async ({  purpose, to }: {  purpose:string;
     return response.data;
 };
 
-export const verifyCode = async ({ email, code }: { email: string; code: string; }): Promise<{ success: boolean }> => {
-    const data = { email, code };
-    const response = await api.post('http://localhost:8080/api/verify-code', data, {
+export const verifyCode = async ({purpose, code}: { purpose: string; code: string; }): Promise<{
+    success: boolean
+}> => {
+    const data = {purpose, code};
+    const response = await api.post('/send-mail/code/verify', data, {
         headers: {
             'Content-Type': 'application/json'
         }
     });
+    console.log(response.data)
 
     return response.data;
 };
