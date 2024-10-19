391180deaa954ba6bf90b0f8e5e08b9587f64b78
박민규 <gksktl111@naver.com>
2024-10-19 16:05:17 +0900

    feat: LoadingSpinner 라이브러리 적용

--- frontend/src/utils/reactQuery/LoadingSpinner.tsx
+++ frontend/src/utils/reactQuery/LoadingSpinner.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import styles from './LoadingSpinner.module.css';
+import {ClipLoader, SyncLoader} from "react-spinners";
 
 const LoadingSpinner: React.FC = () => {
     return (
         <div>
             <div className={styles.spinnerContainer}>
-                <div className={styles.spinner}></div>
+                <ClipLoader size={100} color="#0080FF"/>
             </div>
         </div>
     );
