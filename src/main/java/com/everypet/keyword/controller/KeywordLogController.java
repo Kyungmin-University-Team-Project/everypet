089cff40be9fa67623c103a0c56c29b7d8e54be0
yongho9064 <dydgmldydgh@naver.com>
2024-10-29 19:41:53 +0900

    refactor: ResponseEntity로 응답을 보냄

--- src/main/java/com/everypet/keyword/controller/KeywordLogController.java
+++ src/main/java/com/everypet/keyword/controller/KeywordLogController.java
@@ -1,12 +1,11 @@
 
 package com.everypet.keyword.controller;
-import com.everypet.global.util.ResponseEntityUtil;
+
 import com.everypet.keyword.model.vo.KeywordLog;
 import com.everypet.keyword.service.KeywordLogService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,7 +24,7 @@ public class KeywordLogController {
     @ApiOperation(value = "전체 키워드 로그 출력", notes = "전체 키워드 로그를 출력합니다.")
     @PostMapping("/find-all")
     public ResponseEntity<List<KeywordLog>> findAllKeywordLog() {
-        return ResponseEntityUtil.response(keywordLogService.findAllKeywordLog(), HttpStatus.OK);
+        return ResponseEntity.ok(keywordLogService.findAllKeywordLog());
     }
 
 }
