3ef33c09187298905d124c5f2390163e29a08a7e
Leekwanyong <dlrkdid12@gmail.com>
2024-11-25 16:03:10 +0900

    fix: 주소창 오류 수정,

--- frontend/src/pages/moreInformation/ProductInquiry.tsx
+++ frontend/src/pages/moreInformation/ProductInquiry.tsx
@@ -56,7 +56,7 @@ const ProductInquiry: React.FC<ProductInquiryProps> = ({productId}) => {
                 const sort = 'createdDate,desc';
                 const url = `${API_URL}/support/seller/inquiry/list/${productId}?${page}&${size}&${sort}`;
                 const response = await axios.get(url);
-                setListType(response.data.content); // API 응답에서 항목 설정
+                setListType(response.data.content); // API 응답에서 항목 설정12
             } catch (e) {
                 console.error(e);
             }
