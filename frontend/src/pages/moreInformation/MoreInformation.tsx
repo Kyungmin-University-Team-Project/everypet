36b2d685f247cab39b5becb67fa5cf1f81695035
(Leekwanyong) <(dlrkdid12@gmail.com)>
2024-11-25 23:57:34 +0900

    fix: css 수저ㅏㅇ

--- frontend/src/pages/moreInformation/MoreInformation.tsx
+++ frontend/src/pages/moreInformation/MoreInformation.tsx
@@ -131,14 +131,14 @@ const MoreInformation = () => {
 
                                 />
                             </div>
-                            <h2></h2>
+                            <h4>{productList?.productName}</h4>
                             <div className={styles.reviews}>
                                 <span onClick={() => scrollToSection(reviewsRef)} className={styles.review_count}>
                                 리뷰 보기
                                 </span>
                                 <FaAngleRight
 
-/>
+                                />
                             </div>
                             <p className={styles.price_original}>{productList?.productPrice.toLocaleString()}원</p>
                             <p className={styles.dynamic_price}>
